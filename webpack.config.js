d812e3f6340856bf7b0a2354154582b4b04b9e66
Trevor Farlow <tfarlow@rallydev.com>
2015-12-28 22:51:46 -0700

    fixup! Webpack/Babel config battle has been won! I think.

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
     },
     {
       test: /\.css$/,
-      loader: ['style-loader!css-loader']
+      loader: 'style-loader!css-loader'
     }
   ]
   }
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -43,7 +43,7 @@ module.exports = {
     },
     {
       test: /\.css$/,
-      loaders: ['style-loader!css-loader']
+      loader: 'style-loader!css-loader'
     }]
   }
 };
